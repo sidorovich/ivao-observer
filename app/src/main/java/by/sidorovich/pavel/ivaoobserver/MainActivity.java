3334dd3a3a1e5db6a15b26518b9dbc79f67ff6fe
Pavel Sidorovich <p.sidorovich@gmail.com>
2015-04-13 21:32:41 +0300

    First commit

---
+++ app/src/main/java/by/sidorovich/pavel/ivaoobserver/MainActivity.java
@@ -0,0 +1,13 @@
+package by.sidorovich.pavel.ivaoobserver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
