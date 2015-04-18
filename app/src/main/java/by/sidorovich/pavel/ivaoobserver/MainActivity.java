c8cf1777c7a0e4daf5f088de915cb82dfb910f79
Pavel Sidorovich <sp@onliner.by>
2015-04-18 18:31:29 +0300

    Added IVAO logos, stub for download status from IVAO

--- app/src/main/java/by/sidorovich/pavel/ivaoobserver/MainActivity.java
+++ app/src/main/java/by/sidorovich/pavel/ivaoobserver/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity
 {
+    IvaoStatus ivaoStatus;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ivaoStatus = new IvaoStatus(getPreferences(MODE_PRIVATE));
     }
 }
